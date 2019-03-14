9ea82936602e2fdcf54b50f36822191692a9ec3a
whatinda <jamesd.williams@gmail.com>
2019-03-14 16:01:09 -0500

    section 5

--- datingapp.api/Controllers/AuthController.cs
+++ datingapp.api/Controllers/AuthController.cs
@@ -46,6 +46,8 @@ namespace datingapp.api.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            
+            
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
