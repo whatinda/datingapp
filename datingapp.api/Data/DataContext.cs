ad3e994d127a2528b430d87d2f7fb0b54f4a120e
whatinda <jamesd.williams@gmail.com>
2019-03-13 19:08:38 -0500

    inital commit

---
+++ datingapp.api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using datingapp.api.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace datingapp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ datingapp.api/models/Value.cs
@@ -0,0 +1,8 @@
+namespace datingapp.api.models
+{
+    public class Value
+    {
+        public int Id {get; set; }
+        public string Name { get; set; }  
+    }
+}
