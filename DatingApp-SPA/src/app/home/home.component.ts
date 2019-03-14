6a423d497ffe465463f95ab6340e58ff642f14e5
whatinda <jamesd.williams@gmail.com>
2019-03-14 13:56:33 -0500

    sec 4

---
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { template } from '@angular/core/src/render3';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  registerToggle() {
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
+
+}
--- datingapp.api/Controllers/ValuesController.cs
+++ datingapp.api/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace datingapp.api.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
