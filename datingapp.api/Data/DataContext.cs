f18855ec846b0a83480d71592849e5afdbefd7f7
whatinda <jamesd.williams@gmail.com>
2019-03-13 19:43:07 -0500

    meow

--- datingapp.api/Data/DataContext.cs
+++ datingapp.api/Data/DataContext.cs
@@ -7,5 +7,6 @@ namespace datingapp.api.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
