46b2382024c81ff55b76e17c0453c30e760b339e
whatinda <jamesd.williams@gmail.com>
2019-03-24 06:11:46 -0500

    end of 12

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -54,6 +54,11 @@ export class PhotoEditorComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
+        if (photo.isMain) {
+          this.authServices.changeMemberPhoto(photo.url);
+          this.authServices.currentUser.photoUrl = photo.url;
+          localStorage.setItem('user', JSON.stringify(this.authServices.currentUser));
+        }
       }
     };
   }
